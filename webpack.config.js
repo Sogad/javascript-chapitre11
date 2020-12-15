e9a163dd91f2edc41ca1cce9590caf852a4c2c62
Erwan Vallée <erwanp.vallee@gmail.com>
2020-12-15 13:38:36 +0100

    maj

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: path.join(__dirname, "src/index.js"),
+    main: path.join(__dirname, 'src/index.js'),
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js/,
         exclude: /(node_modules)/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "./src/index.html"),
+      template: path.join(__dirname, './src/index.html'),
     }),
   ],
-  stats: "minimal",
-  devtool: "source-map",
-  mode: "development",
+  stats: 'minimal',
+  devtool: 'source-map',
+  mode: 'development',
   devServer: {
     open: false,
-    contentBase: "./dist",
+    contentBase: path.resolve(__dirname, './dist'),
     inline: true,
     port: 4000,
   },
